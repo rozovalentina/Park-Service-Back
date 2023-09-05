19000a32376d74d1cab8fccfb7b8ba4dd833348d
Gabriel Martín <114639790+gams2004@users.noreply.github.com>
2023-09-05 17:05:46 -0500

    Update TipoVehiculoRepository.java

--- parkingApp/src/main/java/co/edu/javeriana/parkingApp/repository/TipoVehiculoRepository.java
+++ parkingApp/src/main/java/co/edu/javeriana/parkingApp/repository/TipoVehiculoRepository.java
@@ -1,5 +1,21 @@
 package co.edu.javeriana.parkingApp.repository;
 
-public class TipoVehiculoRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import co.edu.javeriana.parkingApp.model.TipoVehiculo;
+
+@Repository
+public interface TipoVehiculoRepository extends JpaRepository<TipoVehiculo, Long> {
+    // https://www.baeldung.com/spring-data-derived-queries
+    List<TipoVehiculo> findAllById(int id);
     
-}
+    // JPQL
+    // https://www.baeldung.com/spring-data-jpa-query
+    @Query("SELECT p FROM tipo_vehiculo p WHERE p.id = :id")
+    List<TipoVehiculo> findPisoById(int id);
+
+    void deleteById(int id);
